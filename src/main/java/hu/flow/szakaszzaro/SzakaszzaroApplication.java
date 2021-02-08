8d63b22579c5da02b9c8dd47a0478b1d92d5cfdb
pakaikarolina <pakaikarolina@gmail.com>
2021-02-08 10:35:28 +0100

    open repository

---
+++ src/main/java/hu/flow/szakaszzaro/SzakaszzaroApplication.java
@@ -0,0 +1,13 @@
+package hu.flow.szakaszzaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzakaszzaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzakaszzaroApplication.class, args);
+	}
+
+}
